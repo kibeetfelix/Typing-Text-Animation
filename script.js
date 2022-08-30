22fc62b86e781c6f4619f5157c76fbc690dd6784
kibeetfelix <felixkibeet@gmail.com>
2022-08-30 19:29:00 +0300

    name changing

---
+++ script.js
@@ -0,0 +1,7 @@
+https://cdn.jsdelivr.net/npm/typed.js@2.0.12
+var typed = new Typed(".auto-type", {
+    strings: ["Coding", "Sleeping", "Eating"],
+    typeSpeed: 150,
+    backSpeed: 150,
+    loop: true
+}) 
